2e744f31d0dc111723f244586f5ac86279dd0fee
shan <shan.p@synergenhealth.com>
2020-01-07 15:08:15 +0530

    first commit

---
+++ src/AppBundle/Controller/ScrapingController.php
@@ -0,0 +1,274 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Routing\Annotation\Route;
+use Goutte\Client;
+
+
+class ScrapingController extends DefaultController
+{
+
+    /**
+     * @Route("/get-urls",name="get_urls")
+     */
+    public function getProductUrls(){
+
+        $categoryArray = array(
+            "supplements",
+            "Herbs-Homeopathy",
+            "sports-nutrition",
+            "bath-personal-care",
+            "beauty",
+            "Grocery",
+            "baby-kids",
+            "pets"
+
+        );
+
+        foreach ($categoryArray as $category){
+
+            $productRepo = $this->getRepository('Product');
+            $em = $this->getDoctrine()->getManager();
+            for($i = 1; $i<2;$i++){
+                $client = new Client();
+                $client->setHeader("x-requested-with","XMLHttpRequest");
+                $crawler = $client->request("GET","https://www.iherb.com/c/".$category."?p=".(string)$i."&noi=192");
+                $productUrls = $crawler->filter("div.absolute-link-wrapper > a")->extract(array('href'));
+                if(count($productUrls) == 0){
+                    break;
+                }
+                foreach ($productUrls as $productUrl) {
+                    $strArray = explode("/",$productUrl);
+                    $productId = (integer)end($strArray);
+                    $existingProduct = $productRepo->findOneBy(array('productId'=> $productId));
+                    if($existingProduct == null){
+                        $product = new Product();
+                        $product->setUrl($productUrl);
+                        $product->setProductId($productId);
+                        $em->persist($product);
+                    }
+
+                }
+
+                $em->flush();
+                var_dump($i);
+            }
+
+        }
+
+
+
+
+
+
+        var_dump(count($productUrls));exit;
+    }
+
+    /**
+     * @Route("/test",name="test")
+     */
+    public function test(){
+
+        $products = $this->getRepository('Product')->findBy(array('title'=>null));
+//        $products = $this->getRepository('Product')->findBy(array('stock'=>0));
+//        var_dump(count($products));exit;
+        foreach ($products as $product){
+            try{
+                $this->scrapeProduct($product);
+            }catch (\Exception $e){
+                var_dump($e->getMessage());exit;
+            }
+
+        }
+        exit;
+    }
+
+    private function scrapeProduct(Product $product){
+        $client = new Client();
+        var_dump($product->getUrl());
+        $client->setHeader("x-requested-with","XMLHttpRequest");
+        $crawler = $client->request("GET",$product->getUrl());
+        $title = $crawler->filter('h1#name')->text();
+        $quantities = $crawler->filter('select[name="qty"] > option');
+        $description = $crawler->filter("div.product-overview > div > section > div.inner-content > div.item-row > div.row > div > div.row");
+
+        $descriptionText = "";
+        $descriptionText.="<div class='row item-row'>".$description->eq(0)->html()."</div>";
+        $descriptionText.="<div class='row item-row'>".$description->eq(1)->html()."</div>";
+        $descriptionText.="<div class='row item-row'>".$description->eq(2)->html()."</div>";
+        $descriptionText.="<div class='row item-row'>".$description->eq(3)->html()."</div>";
+
+        $table = $crawler->filter("div.supplement-facts-container")->html();
+
+        $descriptionText.="<div class='row item-row'>".$table."</div>";
+
+        $stockQuantity = 0;
+        foreach ($quantities as $quantity){
+            $stockQuantity = $quantity->nodeValue;
+            var_dump($stockQuantity);
+        }
+
+        $stockQuantity = (int) $stockQuantity;
+
+        if($stockQuantity < 5){
+            $stockQuantity = 0;
+        }
+
+        $brand = $crawler->filter('meta[property="og:brand"]')->attr("content");
+        $category = $crawler->filter('meta[itemprop="category"]')->attr("content");
+
+
+        $images = $crawler->filter("div.thumbnail-container > img");
+        $imagesList = array();
+        foreach ($images as $image){
+            $imagesList[] = str_replace("/t/","/l/",$image->attributes->getNamedItem('src')->value);
+        }
+
+
+
+        $specs = $crawler->filter("input#modelProperties");
+
+
+        $price = (string)((float) substr($specs->attr("data-list-price"),1)*2);
+        $weight = $specs->attr("data-actual-weight-lb");
+        $sku = $specs->attr("data-part-number");
+        $description = $crawler->filter("div.product-overview > div > section > div.inner-content")->html();
+        $upc = $crawler->filter('span[itemprop="gtin12"]')->text();
+
+        $product->setTitle($title);
+        $product->setDescription($descriptionText);
+        $product->setSku($sku);
+        $product->setUpc($upc);
+        $product->setWeight($weight);
+        $product->setPrice($price);
+        $product->setStock($stockQuantity);
+        $product->setBrand($brand);
+        $product->setCategory($category);
+        $product->setImages($imagesList);
+
+        $this->insert($product);
+
+    }
+
+    /**
+     * @Route("/sync", name="sync")
+     */
+    public function syncAction()
+    {
+        $products = $this->getRepository('Product')->findBy(array('shopifyProductId' => null));
+//        var_dump(count($products));exit;
+        foreach ($products as $product) {
+            $productObj = array();
+            $productObj['title'] = $product->getTitle();
+            $productObj['published'] = false;
+
+            $variantObj = array();
+            $images = array();
+
+            $productObj['product_type'] = $product->getCategory();
+
+            $variantObj['price'] = $product->getPrice();
+            $variantObj['sku'] = $product->getSku();
+            $weight = explode(" ",$product->getWeight());
+            $variantObj['weight'] = $weight[0];
+            $variantObj['weight_unit'] = 'lb';
+            $variantObj['barcode'] = $product->getUpc();
+            $variantObj['inventory_quantity'] = $product->getStock();
+//            $variantObj['inventory_quantity'] = ;
+            $variantObj['inventory_management'] = 'shopify';
+            foreach ($product->getImages() as $image){
+                $images[] =  array('src'=>$image);
+            }
+
+
+            $variantObj['title'] = $product->getTitle();
+            $productObj['vendor'] = $product->getBrand();
+
+
+
+            $productObj['body_html'] = $product->getDescription();
+            $productObj['variants'] = [$variantObj];
+            $productObj['images'] = $images;
+
+            $this->addProductToShopify($productObj,$product);
+
+
+
+        }
+        var_dump(count($products));
+        exit;
+
+    }
+
+    private function addProductToShopify($product,Product $productObj){
+
+        $curl = curl_init();
+
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://f8d419916ee88328f8cf284908c5ee85:cc99e1810cf828f55a9175bcbebe94a6@deep-discount-center.myshopify.com/admin/api/2019-10/products.json",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode(array('product' => $product)),
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json"
+//                "Authorization: Basic NzdmYWY1Y2M2ZDBiZmE0ODM2NDIxODc5NDcwNWJjMGI6NzNlMDFiYjUyYjdmMzhjYzAyOTRmMDRlOGZkZDYxZjI="
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response = json_decode($response, true);
+        $productId = $response['product']['id'];
+        $variantId = $response['product']['variants'][0]['id'];
+        $productObj->setShopifyProductId($productId);
+        $productObj->setShopifyVariantId($variantId);
+        $this->insert($productObj);
+        var_dump($productId);
+        var_dump($variantId);
+
+    }
+
+    private function updateProductInShopify($product, $productId){
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://f8d419916ee88328f8cf284908c5ee85:cc99e1810cf828f55a9175bcbebe94a6@deep-discount-center.myshopify.com/admin/api/2019-10/products/".$productId.".json",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_POSTFIELDS => json_encode(array('product' => $product)),
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json"
+//                "Authorization: Basic NzdmYWY1Y2M2ZDBiZmE0ODM2NDIxODc5NDcwNWJjMGI6NzNlMDFiYjUyYjdmMzhjYzAyOTRmMDRlOGZkZDYxZjI="
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        var_dump($response);exit;
+        curl_close($curl);
+        $response = json_decode($response, true);
+        $productId = $response['product']['id'];
+        $variantId = $response['product']['variants'][0]['id'];
+        $product->setShopifyProductId($productId);
+        $product->setShopifyVariantId($variantId);
+        $this->insert($product);
+        var_dump($productId);
+        var_dump($variantId);
+        exit;
+    }
+}
---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,473 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use http\Params;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=500, unique=true)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="product_id", type="integer", unique=true)
+     */
+    private $productId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sku", type="string", length=255, nullable=true)
+     */
+    private $sku;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="upc", type="string", length=255, nullable=true)
+     */
+    private $upc;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="string",length=255, nullable=true)
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stock", type="integer", nullable=true)
+     */
+    private $stock;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="weight", type="string", length=255, nullable=true)
+     */
+    private $weight;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="brand", type="string", length=255, nullable=true)
+     */
+    private $brand;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255, nullable=true)
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="images", type="text", nullable=true)
+     */
+    private $images;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="shopify_product_id", type="string", length=255, nullable=true)
+     */
+    private $shopifyProductId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="shopify_variant_id", type="string", length=255, nullable=true)
+     */
+    private $shopifyVariantId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Product
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set productId
+     *
+     * @param integer $productId
+     *
+     * @return Product
+     */
+    public function setProductId($productId)
+    {
+        $this->productId = $productId;
+
+        return $this;
+    }
+
+    /**
+     * Get productId
+     *
+     * @return integer
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Product
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set sku
+     *
+     * @param string $sku
+     *
+     * @return Product
+     */
+    public function setSku($sku)
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    /**
+     * Get sku
+     *
+     * @return string
+     */
+    public function getSku()
+    {
+        return $this->sku;
+    }
+
+    /**
+     * Set upc
+     *
+     * @param string $upc
+     *
+     * @return Product
+     */
+    public function setUpc($upc)
+    {
+        $this->upc = $upc;
+
+        return $this;
+    }
+
+    /**
+     * Get upc
+     *
+     * @return string
+     */
+    public function getUpc()
+    {
+        return $this->upc;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set stock
+     *
+     * @param integer $stock
+     *
+     * @return Product
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * Get stock
+     *
+     * @return integer
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * Set weight
+     *
+     * @param string $weight
+     *
+     * @return Product
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    /**
+     * Get weight
+     *
+     * @return string
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Set images
+     *
+     * @param array $images
+     *
+     * @return Product
+     */
+    public function setImages($images)
+    {
+        $this->images = join(",",$images);
+        return $this;
+    }
+
+    /**
+     * Get images
+     *
+     * @return array
+     */
+    public function getImages()
+    {
+        if($this->images == null){
+            return [];
+        }
+        return explode(",",$this->images);
+    }
+
+
+
+    /**
+     * Set brand
+     *
+     * @param string $brand
+     *
+     * @return Product
+     */
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    /**
+     * Get brand
+     *
+     * @return string
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     *
+     * @return Product
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set shopifyProductId
+     *
+     * @param string $shopifyProductId
+     *
+     * @return Product
+     */
+    public function setShopifyProductId($shopifyProductId)
+    {
+        $this->shopifyProductId = $shopifyProductId;
+
+        return $this;
+    }
+
+    /**
+     * Get shopifyProductId
+     *
+     * @return string
+     */
+    public function getShopifyProductId()
+    {
+        return $this->shopifyProductId;
+    }
+
+    /**
+     * Set shopifyVariantId
+     *
+     * @param string $shopifyVariantId
+     *
+     * @return Product
+     */
+    public function setShopifyVariantId($shopifyVariantId)
+    {
+        $this->shopifyVariantId = $shopifyVariantId;
+
+        return $this;
+    }
+
+    /**
+     * Get shopifyVariantId
+     *
+     * @return string
+     */
+    public function getShopifyVariantId()
+    {
+        return $this->shopifyVariantId;
+    }
+}
