a5bbff413dcac4d9a1d31bf08548d5f1503f0e45
shan <shan.p@synergenhealth.com>
2020-01-09 00:17:21 +0530

    Fixed issue

--- src/AppBundle/Repository/ProductRepository.php
+++ src/AppBundle/Repository/ProductRepository.php
@@ -16,7 +16,7 @@ class ProductRepository extends \Doctrine\ORM\EntityRepository
         $q  = $qb->select(array('p'))
             ->where(
                 $qb->expr()->gt('p.stock', 0)
-            )->andWhere('p.shopify_product_id != :productId')
+            )->andWhere('p.shopifyProductId != :productId')
             ->setParameter('productId',null)
             ->setMaxResults($limit)
             ->getQuery();
