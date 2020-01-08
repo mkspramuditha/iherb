f61d741b910c915d888524d780922638d5eebffb
shan <shan.p@synergenhealth.com>
2020-01-09 00:16:42 +0530

    fixed query

--- src/AppBundle/Repository/ProductRepository.php
+++ src/AppBundle/Repository/ProductRepository.php
@@ -16,7 +16,8 @@ class ProductRepository extends \Doctrine\ORM\EntityRepository
         $q  = $qb->select(array('p'))
             ->where(
                 $qb->expr()->gt('p.stock', 0)
-            )->andWhere('p.shopify_product_id',null)
+            )->andWhere('p.shopify_product_id != :productId')
+            ->setParameter('productId',null)
             ->setMaxResults($limit)
             ->getQuery();
 
